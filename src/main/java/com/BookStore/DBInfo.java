07331068c43423aa8dd51319ec74ab030ad8fba7
MahanandaD <deshmukhms1398@gmai.com>
2024-02-01 12:59:16 +0530

    app_server-OnlineBookStore

---
+++ src/main/java/com/BookStore/DBInfo.java
@@ -0,0 +1,7 @@
+package com.BookStore;
+
+public interface DBInfo {
+	public static final String dbUrl="jdbc:oracle:thin:@localhost:1521:orcl";
+	public static final String uName="oracl";
+	public static final String pWord="ora";
+}
